c2f33ed5da0baab0f448a1ab77d85b24380a1632
rahman <rahman17wr@gmail.com>
2024-07-19 23:28:25 +0700

    Create a function to prompt player choice

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const getComputerChoice = () => {
     const randomIndex = Math.floor(Math.random() * choice.length);
     return choice[randomIndex];
 };
+
+const getHumanChoice = () => {
+    let userInput = prompt("Choose Rock, Paper or Scissors!").toLowerCase();
+    while (!choice.includes(userInput)) {
+        userInput = prompt("Please choose either Rock, Paper, or Scissors!");
+    }
+    return userInput;
+};
