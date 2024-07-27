7991a308e3223aec5682a1159d7f7e99525eee6b
rahman <rahman17wr@gmail.com>
2024-07-27 20:13:05 +0700

    Refactor the playGame function, now it receives player input from clicked button and change how it display the result from consol to web page

--- script.js
+++ script.js
@@ -5,44 +5,49 @@ const getComputerChoice = () => {
     return choice[randomIndex];
 };
 
-const getHumanChoice = () => {
-    let userInput = prompt("Choose Rock, Paper or Scissors!").toLowerCase();
-    while (!choice.includes(userInput)) {
-        userInput = prompt("Please choose either Rock, Paper, or Scissors!");
-    }
-    return userInput;
-};
-
 const playGame = () => {
-    const rounds = 5;
+    let rounds = 0;
     let humanScore = 0;
     let computerScore = 0;
+    const result = document.querySelector(".result");
+
+    const playerChoiceBtn = document.querySelectorAll("button");
+    playerChoiceBtn.forEach(button => button.addEventListener("click", (e) => {
+        e.preventDefault();
+        let computerSelection = getComputerChoice();
+        let humanSelection = e.target.value;
+        playRound(humanSelection, computerSelection);
+        rounds++;
+
+        if (rounds === 5) {
+            if (humanScore > computerScore) {
+                result.innerHTML += `<p>Congratulation You Win!. Final score - Player: ${humanScore}, Computer: ${computerScore}</p>`;
+            } else if (humanScore < computerScore) {
+                result.innerHTML += `<p>Oh No You Lose!. Final score - Player: ${humanScore}, Computer: ${computerScore}</p>`;
+            } else {
+                result.innerHTML += `<p>The game is a tie!. Final score - Player: ${humanScore}, Computer: ${computerScore}</p>`;
+            }
+            playerChoiceBtn.forEach(button => button.disabled = true)
+        }
+    }))
 
     const playRound = (humanChoice, computerChoice) => {
+        let roundResult;
         if (humanChoice === computerChoice) {
-            console.log(`Is a tie! Both choose ${humanChoice}. Current score - Player: ${humanScore}, Computer: ${computerScore}`)
+            roundResult = `<p>Round: ${rounds + 1}. Is a tie! Both choose ${humanChoice}. Current score - Player: ${humanScore}, Computer: ${computerScore}</p>`;
         } else if (
             (humanChoice === "rock" && computerChoice === "scissors") ||
             (humanChoice === "paper" && computerChoice === "rock") ||
             (humanChoice === "scissors" && computerChoice === "paper")
         ) {
             humanScore++;
-            console.log(`You win! ${humanChoice} beat ${computerChoice}. Current score - Player: ${humanScore}, Computer: ${computerScore}`);
+            roundResult = `<p>Round: ${rounds + 1}. You win! ${humanChoice} beat ${computerChoice}. Current score - Player: ${humanScore}, Computer: ${computerScore}</p>`;
         } else {
             computerScore++;
-            console.log(`You lose! ${humanChoice} loses to ${computerChoice}. Current score - Player: ${humanScore}, Computer: ${computerScore}`);
+            roundResult = `<p>Round: ${rounds + 1}. You lose! ${humanChoice} loses to ${computerChoice}. Current score - Player: ${humanScore}, Computer: ${computerScore}</p>`;
         }
-    };
-
-    console.log("start");
-
-    if (humanScore > computerScore) {
-        console.log(`Congratulation You Win!. Final score - Player: ${humanScore}, Computer: ${computerScore}`);
-    } else if (humanScore < computerScore) {
-        console.log(`Oh No You Lose!. Final score - Player: ${humanScore}, Computer: ${computerScore}`);
-    } else {
-        console.log(`The game is a tie!. Final score - Player: ${humanScore}, Computer: ${computerScore}`);
-    }
+        result.innerHTML += roundResult;
+    };  
 };
 
 playGame();
